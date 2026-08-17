3cd2d952ad1c0555e5f8c37c8aaee484dcc2dcf8
CkdeCeca <sergiogceca@gmail.com>
2026-08-17 12:29:46 +0200

    Update estimate.js

--- api/estimate.js
+++ api/estimate.js
@@ -82,7 +82,7 @@ export default async function handler(req, res) {
           }
         ],
         tools: [
-          { googleSearch: {} }
+         { google_search: {} }
         ]
       })
     });
